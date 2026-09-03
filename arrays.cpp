a0d7d3d65a41ace3f1230c5dd292324031b5d09d
paiqpaikpaiq <faiqibni195@gmail.com>
2026-09-03 09:22:58 +0700

    Create arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string buah[3] = {"Apel", "Mangga", "Jeruk"};
+
+    for (int i = 0; i < 3; i++) {
+        cout << buah[i] << endl;
+    }
+
+    return 0;
+}
