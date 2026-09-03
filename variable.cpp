dc3d0f233dbd7b4f1af33bd46de30fe57638153e
paiqpaikpaiq <faiqibni195@gmail.com>
2026-09-03 08:09:44 +0700

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int umur = 16;
+    int kelas = 10;
+    int jumlahTeman = 31;
+
+    cout << "Umur         : " << umur << endl;
+    cout << "Kelas        : " << kelas << endl;
+    cout << "Jumlah teman : " << jumlahTeman << endl;
+
+    return 0;
+}
