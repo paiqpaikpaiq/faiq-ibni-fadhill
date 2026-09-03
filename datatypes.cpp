50fa7efc51d6e57d757c0a9c4ca9ef2377eaa4d4
paiqpaikpaiq <faiqibni195@gmail.com>
2026-09-03 08:10:26 +0700

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int jumlahBuku = 10;
+    float hargaBuku = 25000.5;
+    char kodeBuku = 'B';
+
+    cout << "Jumlah buku : " << jumlahBuku << endl;
+    cout << "Harga buku  : " << hargaBuku << endl;
+    cout << "Kode buku   : " << kodeBuku << endl;
+
+    return 0;
+}
