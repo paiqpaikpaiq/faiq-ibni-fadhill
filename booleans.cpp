1ccd847384114af2ade91ee3d8f17fb82d971a56
paiqpaikpaiq <faiqibni195@gmail.com>
2026-09-03 09:29:54 +0700

    Create booleans.cpp

---
+++ booleans.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool lampuMenyala = true;
+
+    if (lampuMenyala) {
+        cout << "Lampu sedang menyala";
+    } else {
+        cout << "Lampu mati";
+    }
+
+    return 0;
+}
