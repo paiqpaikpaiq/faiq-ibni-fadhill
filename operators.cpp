2983d0d3a151d0e3e0dcd1d1bf7b9cd53636c57f
paiqpaikpaiq <faiqibni195@gmail.com>
2026-09-03 08:10:59 +0700

    Create operators.cpp

---
+++ operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int harga = 20000;
+    int jumlah = 3;
+
+    int total = harga * jumlah;
+
+    cout << "Harga barang : Rp" << harga << endl;
+    cout << "Jumlah barang: " << jumlah << endl;
+    cout << "Total harga  : Rp" << total << endl;
+
+    return 0;
+}
