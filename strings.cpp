8bfcf3369ab87b3978c69971b4ddab2cf2d37e1e
paiqpaikpaiq <faiqibni195@gmail.com>
2026-09-03 08:09:19 +0700

    Create strings.cpp

---
+++ strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nama = "Faiq ibni fadhill ";
+    string sekolah = "SMK BINA MANDIRI";
+
+    cout << "Nama    : " << nama << endl;
+    cout << "Sekolah : " << sekolah << endl;
+
+    return 0;
+}
