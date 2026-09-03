6873b0b093745be5bf377978764c25b86489e673
paiqpaikpaiq <faiqibni195@gmail.com>
2026-09-03 09:20:23 +0700

    Create ifelse.cpp

---
+++ ifelse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai;
+    cin >> nilai;
+
+    if (nilai >= 75) {
+        cout << "Lulus";
+    } else {
+        cout << "Tidak Lulus";
+    }
+
+    return 0;
+}
