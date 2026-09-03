c1853ac8cc4ba0bdcb450d72166e944f5812a9f7
paiqpaikpaiq <faiqibni195@gmail.com>
2026-09-03 09:31:03 +0700

    Create userinput.cpp

---
+++ userinput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Masukkan angka pertama: ";
+    cin >> a;
+
+    cout << "Masukkan angka kedua: ";
+    cin >> b;
+
+    cout << "Jumlah = " << a + b;
+
+    return 0;
+}
